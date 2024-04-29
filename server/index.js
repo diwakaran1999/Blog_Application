047c0652120f59090a5b0fd8e3583e2b13710c61
Diwakaran Agrawal <diwakaranagr1999@gmail.com>
2024-04-29 21:29:28 +0530

    update

--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ const PORT = 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
+
 Connection(username, password);
 
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
